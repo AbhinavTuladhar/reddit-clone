b675c6746e96266e667900aa8c732cc0727b4938
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2023-09-06 11:16:34 +0545

    make preliminary schemas

---
+++ src/models/User.ts
@@ -0,0 +1,42 @@
+import { Schema, model, models } from "mongoose";
+
+const UserSchema = new Schema({
+  userName: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true
+  }, 
+  password: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  bio: {
+    type: String,
+  },
+  karma: {
+    postKarma: {
+      type: Number,
+      default: 0,
+    }, 
+    commentKarma: {
+      type: Number,
+      default: 0
+    }
+  },
+  subscribedSubs: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Subreddit'
+    }
+  ]
+})
+
+export default models.User || model('User', UserSchema)
