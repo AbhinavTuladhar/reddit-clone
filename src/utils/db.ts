95533cdd0c0ff7a0968fef8fc33fd164917987d5
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2023-10-24 17:34:18 +0545

    rework database stuff

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -2,13 +2,14 @@ import mongoose from 'mongoose'
 
 export const connectDatabase = async () => {
   try {
-    const url = process.env.MONGODB_URL
+    const url = process.env.MONGODB_URI
     if (!url) {
       console.error('Database url not configured.')
       process.exit(200)
     }
     await mongoose.connect(url)
   } catch (error) {
-    throw new Error('Unable to connect to database')
+    console.error(error)
+    throw error
   }
 }
