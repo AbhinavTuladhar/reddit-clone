5f3dd9d356a3016ae1305a9cd9509851195c9d75
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2024-01-28 12:30:19 +0545

    refactor post detail

--- src/app/r/[subreddit]/comments/[id]/page.tsx
+++ src/app/r/[subreddit]/comments/[id]/page.tsx
@@ -1,34 +1,37 @@
 'use client'
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 import { PiArrowFatUpFill, PiArrowFatDownFill } from 'react-icons/pi'
 import { PostType, voteStatus } from '@/types/types'
+import { Types } from 'mongoose'
 import axios from 'axios'
-import Link from 'next/link';
+import Link from 'next/link'
 import { FaRegCommentAlt } from 'react-icons/fa'
 import { FiGift } from 'react-icons/fi'
 import { PiShareFatBold } from 'react-icons/pi'
-import { useSession } from 'next-auth/react';
-import CommentCard from '@/components/CommentCard';
+import { useSession } from 'next-auth/react'
+import CommentCard from '@/components/CommentCard'
 import useSWR from 'swr'
-import classnames from 'classnames';
-import calculateDateString from '@/utils/calculateDateString';
-import AboutCommunity from '@/components/AboutCommunity';
+import classnames from 'classnames'
+import calculateDateString from '@/utils/calculateDateString'
+import AboutCommunity from '@/components/AboutCommunity'
 import { BsThreeDots } from 'react-icons/bs'
-import useVote from '@/hooks/useVote';
+import useVote from '@/hooks/useVote'
 
 interface SubredditCommentParams {
   params: {
-    subreddit: string,
+    subreddit: string
     id: string
   }
 }
 
+interface PostWithId extends PostType {
+  _id: Types.ObjectId
+  topLevelComments: Types.ObjectId[]
+}
 
-const Page: React.FC<SubredditCommentParams> = ({
-  params,
-}) => {
-  const subredditName = params.subreddit;
+const Page: React.FC<SubredditCommentParams> = ({ params }) => {
+  const subredditName = params.subreddit
   const postId = params.id
 
   const session = useSession()
@@ -40,7 +43,10 @@ const Page: React.FC<SubredditCommentParams> = ({
   const userName = session?.data?.user?.name || ''
 
   const fetcher = (url: string) => fetch(url).then((res) => res.json())
-  const { data, mutate } = useSWR<PostType | null>(`/api/post/${postId}`, fetcher)
+  const { data, mutate } = useSWR<PostWithId | null>(
+    `/api/post/${postId}`,
+    fetcher
+  )
 
   const {
     _id,
@@ -52,13 +58,16 @@ const Page: React.FC<SubredditCommentParams> = ({
     upvotedBy = [],
     downvotedBy = [],
     comments,
-    topLevelComments = []
+    topLevelComments = [],
   } = data || {}
 
   const iconClassName = 'text-reddit-placeholder-gray font-bold w-5 h-5'
 
   const iconBarData = [
-    { icon: <FaRegCommentAlt className={iconClassName} />, label: `${comments?.length || 0} comments` },
+    {
+      icon: <FaRegCommentAlt className={iconClassName} />,
+      label: `${comments?.length || 0} comments`,
+    },
     { icon: <FiGift className={iconClassName} />, label: 'Award' },
     { icon: <PiShareFatBold className={iconClassName} />, label: 'Share' },
   ]
@@ -75,9 +84,19 @@ const Page: React.FC<SubredditCommentParams> = ({
   }
 
   const apiUrl = `/api/post/${_id}`
-  const { voteStatus, setVoteStatus, handleVoteChange } = useVote({ author, apiUrl, initialVoteStatus, mutate, status, userName })
+  const { voteStatus, setVoteStatus, handleVoteChange } = useVote({
+    author,
+    apiUrl,
+    initialVoteStatus,
+    mutate,
+    status,
+    userName,
+  })
 
-  const effectiveKarma = upvotedBy.length + downvotedBy.length === 0 ? 1 : upvotedBy.length - downvotedBy.length + 1
+  const effectiveKarma =
+    upvotedBy.length + downvotedBy.length === 0
+      ? 1
+      : upvotedBy.length - downvotedBy.length + 1
   const dateString = calculateDateString(new Date(createdAt), new Date())
   const paragraphs = body?.split('\n')
 
@@ -91,18 +110,13 @@ const Page: React.FC<SubredditCommentParams> = ({
       return
     }
 
-    // const fetchData = async () => {
-    //   const responses: CommentProps[] = await Promise.all(
-    //     comments.map(async (comment: string) => await axios.get(`/api/comment/${comment}`).then(response => response.data))
-    //   )
-    //   setCommentData(responses)
-    // }
-    // fetchData()
-    setCommentData(topLevelComments)
+    setCommentData(topLevelComments.map((comment) => comment.toString()))
   }, [topLevelComments, comments])
 
   const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    const { target: { value } } = event
+    const {
+      target: { value },
+    } = event
     setComment(value)
   }
 
@@ -120,22 +134,25 @@ const Page: React.FC<SubredditCommentParams> = ({
   }
 
   const commentForm = (
-    <form className='flex flex-col flex-1 my-2 gap-y-2' onSubmit={handleSubmit}>
-      <span className='text-sm'>
+    <form className="flex flex-col flex-1 my-2 gap-y-2" onSubmit={handleSubmit}>
+      <span className="text-sm">
         Comment as&nbsp;
-        <Link href={`/u/${userName}`} className='text-blue-500 duration-300 hover:underline hover:text-red-500'>
+        <Link
+          href={`/u/${userName}`}
+          className="text-blue-500 duration-300 hover:underline hover:text-red-500"
+        >
           {userName}
         </Link>
       </span>
       <textarea
-        className='max-w-full px-4 py-2 h-32 border-[1px] border-reddit-border bg-reddit-dark resize-none placeholder:text-reddit-placeholder-gray'
-        placeholder='What are your thoughts?'
+        className="max-w-full px-4 py-2 h-32 border-[1px] border-reddit-border bg-reddit-dark resize-none placeholder:text-reddit-placeholder-gray"
+        placeholder="What are your thoughts?"
         onChange={handleChange}
         value={comment}
       />
-      <div className='bg-reddit-gray -mt-1.5 px-2 py-1 flex flex-row justify-end'>
+      <div className="bg-reddit-gray -mt-1.5 px-2 py-1 flex flex-row justify-end">
         <button
-          className='px-2 py-1 text-sm bg-white rounded-full disabled:text-gray-400 enabled:text-black disabled:hover:cursor-not-allowed'
+          className="px-2 py-1 text-sm bg-white rounded-full disabled:text-gray-400 enabled:text-black disabled:hover:cursor-not-allowed"
           disabled={comment === ''}
         >
           Comment
@@ -144,13 +161,14 @@ const Page: React.FC<SubredditCommentParams> = ({
     </form>
   )
 
-  const baseIconClassName = 'flex flex-row items-center w-5 h-5 hover:cursor-pointer hover:bg-reddit-hover-gray'
+  const baseIconClassName =
+    'flex flex-row items-center w-5 h-5 hover:cursor-pointer hover:bg-reddit-hover-gray'
 
   return (
-    <main className='flex flex-col lg:flex-row mt-4 gap-4'>
-      <section className='flex flex-col flex-1 w-full px-2 py-4 mx-auto border md:px-4 gap-y-6 bg-reddit-dark border-reddit-border'>
-        <div className='flex flex-row gap-x-4'>
-          <section className='flex flex-col items-center gap-y-1'>
+    <main className="flex flex-col lg:flex-row mt-4 gap-4">
+      <section className="flex flex-col flex-1 w-full px-2 py-4 mx-auto border md:px-4 gap-y-6 bg-reddit-dark border-reddit-border">
+        <div className="flex flex-row gap-x-4">
+          <section className="flex flex-col items-center gap-y-1">
             <PiArrowFatUpFill
               className={classnames(
                 baseIconClassName,
@@ -164,7 +182,7 @@ const Page: React.FC<SubredditCommentParams> = ({
               className={classnames(
                 { 'text-reddit-placeholder-gray': voteStatus === 'nonvoted' },
                 { 'text-reddit-orange': voteStatus === 'upvoted' },
-                { 'text-indigo-400': voteStatus === 'downvoted' },
+                { 'text-indigo-400': voteStatus === 'downvoted' }
               )}
             >
               {effectiveKarma}
@@ -179,57 +197,78 @@ const Page: React.FC<SubredditCommentParams> = ({
               onClick={() => handleVoteChange('downvoted')}
             />
           </section>
-          <section className='flex flex-col flex-1 gap-y-2'>
-
-            <div className='flex flex-row flex-wrap items-center text-xs text-gray-400 gap-x-1'>
-              <Link className='font-bold text-white duration-300 hover:underline' href={`/r/${subreddit}`}> {`r/${subreddit}`}</Link>
+          <section className="flex flex-col flex-1 gap-y-2">
+            <div className="flex flex-row flex-wrap items-center text-xs text-gray-400 gap-x-1">
+              <Link
+                className="font-bold text-white duration-300 hover:underline"
+                href={`/r/${subreddit}`}
+              >
+                {' '}
+                {`r/${subreddit}`}
+              </Link>
               <span> Posted by </span>
-              <Link href={`/u/${author}`} className='duration-300 hover:underline'> {`u/${author}`} </Link>
+              <Link
+                href={`/u/${author}`}
+                className="duration-300 hover:underline"
+              >
+                {' '}
+                {`u/${author}`}{' '}
+              </Link>
               <span> {dateString} </span>
             </div>
-            <h1 className='text-xl font-bold'>
-              {title}
-            </h1>
+            <h1 className="text-xl font-bold">{title}</h1>
             {paragraphs?.map((row, index) => (
               <div key={index}>
                 {row} <br />
               </div>
             ))}
 
-            <section className='flex flex-row gap-x-2'>
+            <section className="flex flex-row gap-x-2">
               {iconBarData.map((row, index) => (
-                <div className='flex flex-row items-center p-2 duration-300 gap-x-2 hover:cursor-pointer hover:bg-reddit-hover-gray' key={index}>
+                <div
+                  className="flex flex-row items-center p-2 duration-300 gap-x-2 hover:cursor-pointer hover:bg-reddit-hover-gray"
+                  key={index}
+                >
                   <span> {row.icon} </span>
-                  <span className='text-reddit-placeholder-gray'> {row.label} </span>
+                  <span className="text-reddit-placeholder-gray">
+                    {' '}
+                    {row.label}{' '}
+                  </span>
                 </div>
               ))}
             </section>
 
-            <>
-              {status === 'authenticated' && commentForm}
-            </>
+            <>{status === 'authenticated' && commentForm}</>
           </section>
         </div>
 
         <div className="flex items-center py-5">
           <div className="flex-grow border-t border-gray-100"></div>
-          <span className="flex justify-end flex-shrink ml-4 text-white"> {comments?.length} comments </span>
+          <span className="flex justify-end flex-shrink ml-4 text-white">
+            {' '}
+            {comments?.length} comments{' '}
+          </span>
         </div>
 
         <>
           {commentData?.map((comment, index) => (
-            <CommentCard id={comment} postAuthor={author || ''} showReply={true} key={index} />
+            <CommentCard
+              id={comment}
+              postAuthor={author || ''}
+              showReply={true}
+              key={index}
+            />
           ))}
         </>
       </section>
 
-      <section className='w-full lg:w-80'>
-        <div className='sticky top-16'>
+      <section className="w-full lg:w-80">
+        <div className="sticky top-16">
           <AboutCommunity subName={subredditName} />
         </div>
       </section>
     </main>
-  );
-};
+  )
+}
 
-export default Page;
+export default Page
--- src/app/r/[subreddit]/page.tsx
+++ src/app/r/[subreddit]/page.tsx
@@ -1,50 +1,64 @@
 'use client'
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 import { SubredditType, PostType } from '@/types/types'
-import PostCard from '@/components/PostCard';
+import PostCard from '@/components/PostCard'
 import SubIcon from '../../../images/subreddit_icon.png'
-import Image from 'next/image';
-import formatSubName from '@/utils/formatSubName';
-import CreatePostCard from '@/components/CreatePostCard';
-import AboutCommunity from '@/components/AboutCommunity';
-import { useSession } from 'next-auth/react';
-import useSWR from 'swr';
+import Image from 'next/image'
+import formatSubName from '@/utils/formatSubName'
+import CreatePostCard from '@/components/CreatePostCard'
+import AboutCommunity from '@/components/AboutCommunity'
+import { useSession } from 'next-auth/react'
+import useSWR from 'swr'
 import axios from 'axios'
-import { JoinSubBody } from '@/types/types';
-import classnames from 'classnames';
+import { JoinSubBody } from '@/types/types'
+import classnames from 'classnames'
+import { Types } from 'mongoose'
 
 interface SubredditParams {
   params: {
-    subreddit: string;
+    subreddit: string
   }
 }
 
 type JoinStatusType = 'Join' | 'Joined'
 
+interface PostTypeWithId extends PostType {
+  _id: Types.ObjectId
+}
+
 const Page: React.FC<SubredditParams> = ({ params }) => {
-  const subredditName = params.subreddit;
+  const subredditName = params.subreddit
   const formattedSubredditName = formatSubName(subredditName)
   const session = useSession()
   const { status } = session
   const userName = session?.data?.user?.name || ''
 
-  const fetcher = (url: string) => axios.get(url).then((response) => response.data)
+  const fetcher = (url: string) =>
+    axios.get(url).then((response) => response.data)
   const multiFetcher = (urls: string[]) => {
-    return Promise.all(urls.map(url => fetcher(url)))
+    return Promise.all(urls.map((url) => fetcher(url)))
   }
 
   // For some reason multifethcer function all of a sudden started showing an error (:|
-  const { data: subInfo, mutate: mutateSubInfo } = useSWR<SubredditType | null>(`/api/r/${subredditName}`, fetcher)
-  const { data: postDetails } = useSWR<PostType[]>(
-    subInfo ? subInfo.posts.map((post: string) => `/api/post/${post}`) : [],
+  const { data: subInfo, mutate: mutateSubInfo } = useSWR<SubredditType | null>(
+    `/api/r/${subredditName}`,
+    fetcher
+  )
+  const { data: postDetails } = useSWR<PostTypeWithId[]>(
+    subInfo ? subInfo.posts.map((post) => `/api/post/${post.toString()}`) : [],
     // @ts-ignore
     multiFetcher
   )
 
   // Check if the user has jointed the sub
-  const initialJoinStatus: JoinStatusType = subInfo?.subscribers?.includes(userName) ? 'Joined' : 'Join'
-  const [joinStatus, setJoinStatus] = useState<JoinStatusType>(initialJoinStatus)
+  const initialJoinStatus: JoinStatusType = subInfo?.subscribers?.includes(
+    userName
+  )
+    ? 'Joined'
+    : 'Join'
+  const [joinStatus, setJoinStatus] =
+    useState<JoinStatusType>(initialJoinStatus)
 
   useEffect(() => {
     setJoinStatus(initialJoinStatus)
@@ -52,68 +66,77 @@ const Page: React.FC<SubredditParams> = ({ params }) => {
 
   const handleJoin = async () => {
     const patchRequestBody: JoinSubBody = {
-      subreddit: subredditName, userName: userName
+      subreddit: subredditName,
+      userName: userName,
     }
     await axios.patch('/api/r/join', patchRequestBody)
-    setJoinStatus(prevStatus => (
-      prevStatus === 'Join' ? 'Joined' : 'Join'
-    ))
+    setJoinStatus((prevStatus) => (prevStatus === 'Join' ? 'Joined' : 'Join'))
     mutateSubInfo()
   }
 
   return (
     <div>
-      <div className='w-full h-20 bg-reddit-blue box-border'> </div>
-      <section className='flex flex-row w-full pl-6 -ml-0 gap-x-2 bg-reddit-gray'>
+      <div className="w-full h-20 bg-reddit-blue box-border"> </div>
+      <section className="flex flex-row w-full pl-6 -ml-0 gap-x-2 bg-reddit-gray">
         <Image
           src={SubIcon}
-          className='w-16 h-16 mt-0 sm:w-20 sm:h-20 sm:-mt-5 border-4 rounded-full'
-          alt='sub icon'
+          className="w-16 h-16 mt-0 sm:w-20 sm:h-20 sm:-mt-5 border-4 rounded-full"
+          alt="sub icon"
         />
-        <div className='flex flex-col flex-1 justify-center my-1 gap-y-1'>
-          <div className='flex flex-row flex-wrap items-center justify-between sm:justify-start gap-4'>
-            <h1 className='text-2xl font-bold sm:block hidden'>
+        <div className="flex flex-col flex-1 justify-center my-1 gap-y-1">
+          <div className="flex flex-row flex-wrap items-center justify-between sm:justify-start gap-4">
+            <h1 className="text-2xl font-bold sm:block hidden">
               {formattedSubredditName}
             </h1>
-            <small className='text-base font-bold block sm:hidden'>
+            <small className="text-base font-bold block sm:hidden">
               {`r/${subredditName}`}
             </small>
             {status === 'authenticated' && (
               <button
                 className={classnames(
                   'flex mr-2 items-center px-2 sm:px-6 py-1 text-base font-bold rounded-full hover:cursor-pointer',
-                  { 'text-black bg-reddit-white hover:brightness-90 duration-300': joinStatus === 'Join' },
-                  { 'text-reddit-white bg-transparent border border-reddit-white': joinStatus === 'Joined' }
+                  {
+                    'text-black bg-reddit-white hover:brightness-90 duration-300':
+                      joinStatus === 'Join',
+                  },
+                  {
+                    'text-reddit-white bg-transparent border border-reddit-white':
+                      joinStatus === 'Joined',
+                  }
                 )}
-                onClick={handleJoin}>
+                onClick={handleJoin}
+              >
                 {joinStatus}
               </button>
             )}
           </div>
-          <small className='text-base font-bold hidden sm:block'>
+          <small className="text-base font-bold hidden sm:block">
             {`r/${subredditName}`}
           </small>
         </div>
       </section>
 
       <div className={`flex flex-col-reverse lg:flex-row gap-4 my-4`}>
-        <div className='flex flex-col flex-1 gap-y-0'>
+        <div className="flex flex-col flex-1 gap-y-0">
           {status === 'authenticated' && (
-            <div className='flex flex-col mb-4 gap-y-4'>
+            <div className="flex flex-col mb-4 gap-y-4">
               <CreatePostCard />
             </div>
           )}
           {postDetails?.map((post, index) => (
-            <PostCard id={post._id} subViewFlag={false} key={index} />
+            <PostCard
+              id={post._id.toString()}
+              subViewFlag={false}
+              key={index}
+            />
           ))}
         </div>
-        <section className='w-full lg:w-80'>
+        <section className="w-full lg:w-80">
           <AboutCommunity subName={subredditName} />
         </section>
       </div>
-
     </div>
-  );
-};
+  )
+}
 
-export default Page;
+export default Page
