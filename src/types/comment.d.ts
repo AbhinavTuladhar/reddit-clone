c76c856dc80983521228d0d9c2dc5decb1acfda5
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2025-01-05 21:33:52 +0545

    feat: make services file for comments

--- src/types/comment.d.ts
+++ src/types/comment.d.ts
@@ -1,6 +1,15 @@
+import { Types } from 'mongoose'
+
 export interface CommentEditBody {
   userName: string
   content: string
+}
+
+export interface CommentCreationBody {
+  content: string
+  author: string
+  post: Types.ObjectId
+  parentComment?: string
 }
 
 export { CommentType } from '@/models/Comment'
